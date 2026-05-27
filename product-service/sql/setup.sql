025e43f29eabd3b6115d57fd5e80323ccc9fc50e
王祝融 <17055131+wang-zhurong315@user.noreply.gitee.com>
2026-05-27 10:28:14 +0800

    fix product-service tracking

---
+++ product-service/sql/setup.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE Market;
+USE Market;
+
+CREATE TABLE Product (
+    product_name VARCHAR(50),
+    product_price DECIMAL(10, 2),
+    product_inventory INT,
+    product_id INT PRIMARY KEY AUTO_INCREMENT    kmkmkmk
+);
